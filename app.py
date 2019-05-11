ffb5814435d1bf47424a119cf81999cccee15da3
marez111 <myrealgeorgia@gmail.com>
2019-05-11 01:47:42 -0400

    added start-end route

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ app = Flask(__name__)
 
 # Flask Routes
 
+
 @app.route("/")
 def welcome():
     """List all available api routes."""
@@ -37,18 +38,24 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/start/<start><br/>"
+        f"/api/v1.0/start-end/<start><end>"
     )
+
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     """Precipitation by Date: list of  dictionaries"""
 
-    latest_year = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
-    start_year = dt.datetime.strptime(latest_year, "%Y-%m-%d") - dt.timedelta(days = 365)
+    latest_year = session.query(
+        Measurement.date).order_by(
+        Measurement.date.desc()).first()[0]
+    start_year = dt.datetime.strptime(
+        latest_year, "%Y-%m-%d") - dt.timedelta(days=365)
 
-    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= start_year).all()
+    results = session.query(
+        Measurement.date, Measurement.prcp).filter(
+        Measurement.date >= start_year).all()
     print(f"len(results): {len(results)}")
     print(f"type(results): {type(results)}")
 
@@ -67,22 +74,25 @@ def stations():
     """JSON list of stations"""
 
     results = session.query(Measurement.station)\
-    .group_by(Measurement.station).all()
+        .group_by(Measurement.station).all()
 
     results_list = list(np.ravel(results))
 
     return jsonify(results_list)
 
+
 @app.route("/api/v1.0/tobs")
 def tobs():
     """dates and temperature observations from a year from the last data point."""
 
-    latest_year = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
-    start_year = dt.datetime.strptime(latest_year, "%Y-%m-%d") - dt.timedelta(days = 365)
+    latest_year = session.query(
+        Measurement.date).order_by(
+        Measurement.date.desc()).first()[0]
+    start_year = dt.datetime.strptime(
+        latest_year, "%Y-%m-%d") - dt.timedelta(days=365)
 
-    results = session.query(Measurement.station,Measurement.tobs)\
-    .filter(Measurement.date >= start_year).all()
-
+    results = session.query(Measurement.station, Measurement.tobs)\
+        .filter(Measurement.date >= start_year).all()
 
     tobs_list = []
     for station, tob in results:
@@ -99,7 +109,7 @@ def start(start):
     """Temp_MIN, Temp_AVG, and Temp_MAX for all dates greater than and equal to the start parameter."""
 
     results = session.query(Measurement.date, func.min(Measurement.tobs),
-        func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+                            func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
         filter(Measurement.date >= start).group_by(Measurement.date).all()
 
     temp_stats = []
@@ -116,6 +126,31 @@ def start(start):
     return jsonify(temp_stats)
 
 
+@app.route("/api/v1.0/start-end/<start>/<end>")
+def start_end(start, end):
+    """Temp_MIN, Temp_AVG, and Temp_MAX for dates between start and end.
+        enter a `/` between start and end date
+    """
+
+    results = session.query(Measurement.date, func.min(Measurement.tobs),
+                            func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start)\
+        .filter(Measurement.date <= end)\
+        .group_by(Measurement.date).all()
+
+    temp_stats = []
+
+    for date, tmin, tavg, tmax in results:
+        temps_dict = {}
+        temps_dict["date"] = date
+        temps_dict["tmin"] = tmin
+        temps_dict["tavg"] = tavg
+        temps_dict["tmax"] = tmax
+
+        temp_stats.append(temps_dict)
+
+    return jsonify(temp_stats)
+
 
 if __name__ == "__main__":
-    app.run(host= "localhost", port=5000, debug=True)
+    app.run(host="localhost", port=5000, debug=True)
